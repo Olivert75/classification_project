1db7dd40f588731b4f082cf5863fdd135486ffd9
Olivert75 <oliver.ton75@gmai.com>
2022-03-14 00:21:11 -0500

    add a fucntion

--- modeling.py
+++ modeling.py
@@ -1,15 +1,13 @@
-def get_metrics_binary(clf):
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import pandas as pd
+
+def get_metrics_binary(X_train, y_train, y_pred,clf):
     '''
     This function takes in a confusion matrix (cnf) for a binary classifier and prints out metrics based on
     values in variables named X_train, y_train, and y_pred.
     return: a classification report as a transposed DataFrame
     '''
-    X_train, y_train = train[x_col], train[y_col]
-
-    X_validate, y_validate = validate[x_col], validate[y_col]
-
-    X_test, y_test = test[x_col], validate[y_col]
-
     accuracy = clf.score(X_train, y_train)
     class_report = pd.DataFrame(classification_report(y_train, y_pred, output_dict=True)).T
     conf = confusion_matrix(y_train, y_pred)
@@ -18,8 +16,8 @@ def get_metrics_binary(clf):
     tnr = conf[0][0] / conf[0].sum()
     fnr = conf[1][0] / conf[1].sum()
     print(f'''
-    The accuracy for our model is {accuracy:.4}
-    The True Positive Rate is {tpr:.3}, The False Positive Rate is {fpr:.3},
-    The True Negative Rate is {tnr:.3}, and the False Negative Rate is {fnr:.3}
+    The accuracy for our model is {accuracy:.3%}
+    The True Positive Rate is {tpr:.3%}, The False Positive Rate is {fpr:.3%},
+    The True Negative Rate is {tnr:.3%}, and the False Negative Rate is {fnr:.3%}
     ''')
     return class_report
